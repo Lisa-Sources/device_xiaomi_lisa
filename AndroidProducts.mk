5266e76fd06a8eab028a32773529ca446ae8446f
zaidkhan0997 <kzaid0997@gmail.com>
2023-08-05 14:15:42 +0530

    lisa: Bringup Evolution-X OFFICIAL
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lisa.mk
+    $(LOCAL_DIR)/evolution_lisa.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lisa-user \
-    aosp_lisa-userdebug \
-    aosp_lisa-eng
+    evolution_lisa-user \
+    evolution_lisa-userdebug \
+    evolution_lisa-eng

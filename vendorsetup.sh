d43b0ec1d253a4200e013524303a4ca9e5a65dc1
zaidkhan0997 <kzaid0997@gmail.com>
2023-10-22 22:19:24 +0530

    [TMP}lisa: add vendorsetup.sh
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone -b 14 https://github.com/Lisa-Sources/vendor_xiaomi_lisa.git vendor/xiaomi/lisa
+git clone -b 14 --depth=1 https://github.com/Lisa-Sources/kernel_xiaomi_lisa.git kernel/xiaomi/lisa
